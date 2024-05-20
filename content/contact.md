04ee8cebd3a7ad0c20550f67dd1d71a2d5dc5f62
dave long <davelongdev@gmail.com>
2024-05-20 19:27:20 -0400

    update contact page and contact form

--- content/contact.md
+++ content/contact.md
@@ -4,4 +4,6 @@ date = 2024-05-06T20:27:15-04:00
 draft = false
 type = 'contact'
 +++
-CONTACT PAGE CONTENT
+# Contact Dave Long Web Studio
+
+Feel free to reach out if you're interested in discussing websites, web development, digital publishing, or design, or if you're interested in working together on a project.
---
+++ static/js/form.js
@@ -0,0 +1,49 @@
+// listen for submit event
+form.addEventListener('submit', function (event) {
+
+  // get form
+  let form = document.querySelector('form');
+
+  // set 'action' varialbe to the form's action - here the worker url
+  let action = form.action;
+
+  // set 'method' variable to the form's method - here 'post'
+  let method = form.method;
+
+  // create formdata object (different than regular js object)
+  let fdObject = new FormData(form);
+  // console.log(fdObject)
+
+  // serialize formdata object into regular object
+  let dataObject = Object.fromEntries(fdObject);
+  // console.log(dataObject)
+  // prevent default form behavior, i.e., send post request to action url
+  event.preventDefault();
+
+  // update status element
+  statusElem.textContent = 'Sending your message...';
+
+  // fetch request
+  fetch(action, {
+    method: method,
+    body: JSON.stringify(dataObject),
+    headers: {
+      'Content-type': 'application/json',
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      statusElem.textContent = 'Message sent.';
+      form.reset();
+      return response.json();  
+    } else {
+       statusElem.textContent = 'Something went wrong. Please try again.';
+    }
+  }).then(function (data) {
+    console.log(data);
+  }).catch(function (error) {
+    console.warn(error);
+});
+
+  ;
+
+});
